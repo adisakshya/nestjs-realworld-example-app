a38d9975b6497ed762e74763a35b867b7daaa9a1
adisakshya <adisakshya@triveous.com>
2020-08-30 13:49:05 +0530

    fix: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { ProfileModule } from './profile/profile.module';
 import { TagModule } from './tag/tag.module';
+import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "192.168.99.100",
+      port: 5431,
+      username: "root",
+      password: "root",
+      database: "nestjsrealworld",
+      synchronize: true,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')]
+    }),
     ArticleModule,
     UserModule,
     ProfileModule,
